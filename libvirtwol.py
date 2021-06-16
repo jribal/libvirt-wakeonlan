3a983216a57bb60e162b56c4e2381dddab8e0d4d
Reinout <reinout.eyckerman@uantwerpen.be>
2021-06-16 12:29:41 +0200

    removed python-libpcap dependency, added pcapy and pypacker dependency, reduced complexity

--- libvirtwol.py
+++ libvirtwol.py
@@ -17,14 +17,16 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    dmacias - added fixes for ether proto 0x0842
 
-import pcap
-import sys
+import logging
 import socket
 import struct
-import string
-import libvirt
-import logging
+import sys
 from xml.dom import minidom
+
+import libvirt
+import pcapy
+
+from pypacker.layer12.ethernet import Ethernet
 
 
 class LibVirtWakeOnLan:
@@ -38,7 +40,6 @@ class LibVirtWakeOnLan:
                 macadd = interface.getElementsByTagName("mac")
                 return macadd[0].getAttribute("address")
         return None
-
 
     @staticmethod
     def StartServerByMACAddress(mac):
@@ -71,79 +72,69 @@ class LibVirtWakeOnLan:
 
     @staticmethod
     def GetMACAddress(s):
-            # added fix for ether proto 0x0842
-            size = len(s)
-            bytes = map(lambda x: '%.2x' % x, map(ord, s))
-            counted = 0
-            macpart = 0
-            maccounted = 0
-            macaddress = None
-            newmac = ""
+        # added fix for ether proto 0x0842
+        s=repr(s)[2:-1]
+        size = len(s)
+        bytes = map(lambda x: '%.2x' % x, map(ord, s))
+        counted = 0
+        macpart = 0
+        maccounted = 0
+        macaddress = None
+        newmac = ""
 
-            for byte in bytes:
-                if counted < 6:
-                    # find 6 repetitions of 255 and added fix for ether proto 0x0842
-                    if byte == "ff" or size < 110:
-                        counted += 1
-                else:
-                    # find 16 repititions of 48 bit mac
-                    macpart += 1
-                    if newmac != "":
-                        newmac += ":"
+        for byte in bytes:
+            if counted < 6:
+                # find 6 repetitions of 255 and added fix for ether proto 0x0842
+                if byte == "ff" or size < 110:
+                    counted += 1
+            else:
+                # find 16 repititions of 48 bit mac
+                macpart += 1
+                if newmac != "":
+                    newmac += ":"
 
-                    newmac += byte
+                newmac += byte
 
-                    if macpart is 6 and macaddress is None:
-                        macaddress = newmac
+                if macpart == 6 and macaddress is None:
+                    macaddress = newmac
 
-                    if macpart is 6:
-                        #if macaddress != newmac:
-                            #return None
-                        newmac = ""
-                        macpart = 0
-                        maccounted += 1
+                if macpart == 6:
+                    # if macaddress != newmac:
+                    # return None
+                    newmac = ""
+                    macpart = 0
+                    maccounted += 1
 
-            if counted > 5 and maccounted > 5:
-                return macaddress
+        if counted > 5 and maccounted > 5:
+            return macaddress
 
     @staticmethod
-    def DecodeIPPacket(s):
+    def DecodeIPPacket(s)->Ethernet:
         if len(s) < 20:
             return None
-        d = {}
-        d['version'] = (ord(s[0]) & 0xf0) >> 4
-        d['header_len'] = ord(s[0]) & 0x0f
-        d['tos'] = ord(s[1])
-        d['total_len'] = socket.ntohs(struct.unpack('H', s[2:4])[0])
-        d['id'] = socket.ntohs(struct.unpack('H', s[4:6])[0])
-        d['flags'] = (ord(s[6]) & 0xe0) >> 5
-        d['fragment_offset'] = socket.ntohs(struct.unpack('H', s[6:8])[0] & 0x1f)
-        d['ttl'] = ord(s[8])
-        d['protocol'] = ord(s[9])
-        d['checksum'] = socket.ntohs(struct.unpack('H', s[10:12])[0])
-        d['source_address'] = pcap.ntoa(struct.unpack('i', s[12:16])[0])
-        d['destination_address'] = pcap.ntoa(struct.unpack('i', s[16:20])[0])
-        if d['header_len'] > 5:
-            d['options'] = s[20:4 * (d['header_len'] - 5)]
-        else:
-            d['options'] = None
-        d['data'] = s[4 * d['header_len']:]
-        return d
+        print("len confirmed")
+        decoded=Ethernet(s)
+        return decoded
 
     @staticmethod
-    def InspectIPPacket(pktlen, data, timestamp):
+    def InspectIPPacket(pkthdr, data:str):
+        print(data)
         if not data:
             return
-        decoded = LibVirtWakeOnLan.DecodeIPPacket(data[14:])
-        macaddress = LibVirtWakeOnLan.GetMACAddress(decoded['data'])
+        print("decoding")
+        decoded = LibVirtWakeOnLan.DecodeIPPacket(data)
+        macaddress = LibVirtWakeOnLan.GetMACAddress(decoded.header_bytes)
         if not macaddress:
             return
         return LibVirtWakeOnLan.StartServerByMACAddress(macaddress)
 
+
 if __name__ == '__main__':
     from lvwolutils import Utils
-    Utils.SetupLogging()
 
+    Utils.SetupLogging()
+    data = b'\xff\xff\xff\xff\xff\xff\x94\xc6\x91\xa32\x7f\x08\x00E\x00\x00\x82Vo@\x00@\x117\xce\xac\x10\x00\x1e\xff\xff\xff\xff\x81\xb1\x00\t\x00n\xde\x10\xff\xff\xff\xff\xff\xff@\x8d\\\xb7\xf1\xec@\x8d\\\xb7\xf1\xec@\x8d\\\xb7\xf1\xec@\x8d\\\xb7\xf1\xec@\x8d\\\xb7\xf1\xec@\x8d\\\xb7\xf1\xec@\x8d\\\xb7\xf1\xec@\x8d\\\xb7\xf1\xec@\x8d\\\xb7\xf1\xec@\x8d\\\xb7\xf1\xec@\x8d\\\xb7\xf1\xec@\x8d\\\xb7\xf1\xec@\x8d\\\xb7\xf1\xec@\x8d\\\xb7\xf1\xec@\x8d\\\xb7\xf1\xec@\x8d\\\xb7\xf1\xec'
+    LibVirtWakeOnLan.InspectIPPacket(0,data)
     # line below is replaced on commit
     LVWOLVersion = "20140814 231218"
     Utils.ShowVersion(LVWOLVersion)
@@ -153,16 +144,17 @@ if __name__ == '__main__':
         sys.exit(0)
 
     interface = sys.argv[1]
-    p = pcap.pcapObject()
-    net, mask = pcap.lookupnet(interface)
+    #    p = pcapy.lookupdev()
+    # p = pcap.pcapObject()
+    # net, mask = pcap.lookupnet(interface)
     # set promiscuous to 1 so all packets are captured
-    p.open_live(interface, 1600, 1, 100)
+    reader = pcapy.open_live(interface, 1600, 1, 100)
     # added support for ether proto 0x0842
-    p.setfilter('udp port 7 or udp port 9 or ether proto 0x0842', 0, 0)
+    reader.setfilter('udp port 7 or udp port 9 or ether proto 0x0842')
 
     while True:
         try:
-            p.dispatch(1, LibVirtWakeOnLan.InspectIPPacket)
+            reader.dispatch(1, LibVirtWakeOnLan.InspectIPPacket)
         except KeyboardInterrupt:
             break
         except Exception:
